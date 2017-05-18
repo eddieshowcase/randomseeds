75676b60be9eeafebc58ec388025218da49b5147
Ian Erickson <eddieshowcase@gmail.com>
2017-05-17 21:25:33 -0600

    Grid cards in good order.

--- home.php
+++ home.php
@@ -8,7 +8,7 @@ get_header(); ?>
 <div id="page" class="homepage" role="main">
 	<article class="main-content p0">
 
-<!--		<h1 class="text-center mb-">A portfolio of sorts.</h1>-->
+		<!--		<h1 class="text-center mb-">A portfolio of sorts.</h1>-->
 		<h1 id="quip">
 			<?php
 			$page_id = get_queried_object_id();
@@ -27,29 +27,33 @@ get_header(); ?>
 
 					<?php //get_template_part( 'template-parts/content', get_post_format() ); ?>
 
-					<div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry'); ?>>
+					<div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry border-randomseeds round-corners card-randomseeds'); ?>>
 
 						<a href="<?php the_permalink(); ?>">
 
 						 	<?php //the_post_thumbnail(); ?>
-							<img src="<?php the_post_thumbnail_url(); ?>"/>
+<!--							<img src="--><?php //the_post_thumbnail_url(); ?><!--" class="round-top-corners"/>-->
+<!--							<div class="round-top-corners"
+									 style="background: url(<?php //the_post_thumbnail_url(); ?>); background-size: cover; background-position: center; height: 320px;">
+							</div> -->
 
-							<header>
+							<?php
+								$post_id = get_the_ID();
+								$post_thumb_path = get_post_meta( $post_id, 'thumb', true);
+								$post_thumb = "http://localhost/randomseeds/images/thumbs/".$post_thumb_path;
+							?>
+							<img src="<?php echo $post_thumb ?>"/>
+
+							<header class="p-">
 								<h2 class="mb0"><?php the_title(); ?></h2>
-								Some project 1-sentence summary to go here.
+								<?php echo(get_the_excerpt()); ?>
 							</header>
 
 						</a>
-<!--						<hr />-->
 
-<!--						<div class="entry-content">-->
-<!--							--><?php //the_content( __( 'Continue reading...', 'foundationpress' ) ); ?>
-<!--						</div>-->
-
-
-<!--						<footer>-->
-<!--							--><?php //$tag = get_the_tags(); if ( $tag ) { ?><!--<p>--><?php //the_tags(''); ?><!--</p>--><?php //} ?>
-<!--						</footer>-->
+						<!--<footer>-->
+						<!--	--><?php //$tag = get_the_tags(); if ( $tag ) { ?><!--<p>--><?php //the_tags(''); ?><!--</p>--><?php //} ?>
+						<!--</footer>-->
 
 					</div>
 
